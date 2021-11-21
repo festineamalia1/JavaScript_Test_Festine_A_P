2db6c321c4e261eaa05eb11a7f53ca52d2e71d29
Festine-Capio <festine@capioteknologi.co.id>
2021-11-21 21:15:33 +0700

    update

--- test1.js
+++ test1.js
@@ -2,12 +2,15 @@
 // Direction : Return sum of array
 
 const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const ar = [1,3];
 
 function result(input) {
   // Your Code Here
-for (var i = Array.length; i--; ) {
-  input += Array[i];
-}
+  var sum = 0;
+  for (var i = 0; i < input.length; i++) {
+    sum += input[i];
+  }
+  return sum;
 }
 
 console.log(result(input));
